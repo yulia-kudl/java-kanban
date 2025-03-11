3857752769469ee3a0b515eb8429cd61843f9e49
Yulia Kudlai <iu.5hi@yandex.ru>
2025-03-11 17:44:01 +0300

    code style

--- src/Epic.java
+++ src/Epic.java
@@ -20,7 +20,7 @@ public class Epic extends Task {
 
     public void updateEpicStatus() {
         if (epicSubTasks.isEmpty()) {
-            this.status =TaskStatus.NEW;
+            this.status = TaskStatus.NEW;
             return;
         }
         boolean newTask = false;
@@ -71,7 +71,7 @@ public class Epic extends Task {
                 "name='" + name + '\'' +
                 ", description='" + description + '\'' +
                 ", id=" + id +
-                ", status=" + status + '\''+
+                ", status=" + status + '\'' +
                 "epicSubTasks=" + epicSubTasks +
                 '}';
     }
